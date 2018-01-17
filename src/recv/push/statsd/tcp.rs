273d967a343819fd10a894aa5e241a735aefdda5
Dirk Gadsden <dirk@esherido.com>
2018-01-17 11:00:12 -0600

    Make `StatsdTcpListener` generic with regard to its socket address

--- src/recv/push/statsd/tcp.rs
+++ src/recv/push/statsd/tcp.rs
@@ -8,23 +8,25 @@ use super::parse_metrics;
 use super::super::super::collector::Collector;
 
 /// Listens on a TCP socket for StatsD messages.
-pub struct StatsdTcpListener {
+pub struct StatsdTcpListener<A: ToSocketAddrs> {
     collector: Collector,
+    addr: A,
 }
 
-impl StatsdTcpListener {
-    pub fn new(collector: Collector) -> StatsdTcpListener {
+impl<A: ToSocketAddrs> StatsdTcpListener<A> {
+    pub fn new(collector: Collector, addr: A) -> StatsdTcpListener<A> {
         StatsdTcpListener {
             collector,
+            addr,
         }
     }
 
-    pub fn listen<A: ToSocketAddrs>(&mut self, addr: A) {
+    pub fn listen(&mut self) {
         let (send, recv) = channel();
 
-        let listener = TcpListener::bind(addr).unwrap();
+        let listener = TcpListener::bind(&self.addr).unwrap();
         thread::spawn(move || {
-            StatsdTcpListener::accept_on_listener(listener, send)
+            accept_on_listener(listener, send)
         });
 
         for line in recv {
@@ -40,44 +42,44 @@ impl StatsdTcpListener {
             }
         }
     }
+}
 
-    fn accept_on_listener(listener: TcpListener, send: Sender<String>) {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    // Clients have 30 seconds to send us data before we'll drop.
-                    let _ = stream.set_read_timeout(Some(Duration::from_secs(30)));
+fn accept_on_listener(listener: TcpListener, send: Sender<String>) {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                // Clients have 30 seconds to send us data before we'll drop.
+                let _ = stream.set_read_timeout(Some(Duration::from_secs(30)));
 
-                    let send = send.clone();
+                let send = send.clone();
 
-                    thread::spawn(move || {
-                        StatsdTcpListener::handle_client(stream, send)
-                    });
-                },
-                Err(e) => panic!("Failed to listen on TCP socket: {}", e),
-            }
+                thread::spawn(move || {
+                    handle_client(stream, send)
+                });
+            },
+            Err(e) => panic!("Failed to listen on TCP socket: {}", e),
         }
     }
+}
 
-    fn handle_client(stream: TcpStream, send: Sender<String>) {
-        let mut reader = BufReader::new(stream);
+fn handle_client(stream: TcpStream, send: Sender<String>) {
+    let mut reader = BufReader::new(stream);
 
-        loop {
-            let mut line = String::new();
+    loop {
+        let mut line = String::new();
 
-            match reader.read_line(&mut line) {
-                Err(err) => {
-                    println!("Error reading StatsD line: {:?}", err);
-                    break
-                },
-                Ok(0) => {
-                    // Close if there are no more bytes.
-                    break
-                },
-                Ok(_) => {
-                    send.send(line).unwrap()
-                },
-            }
+        match reader.read_line(&mut line) {
+            Err(err) => {
+                println!("Error reading StatsD line: {:?}", err);
+                break
+            },
+            Ok(0) => {
+                // Close if there are no more bytes.
+                break
+            },
+            Ok(_) => {
+                send.send(line).unwrap()
+            },
         }
-    } // fn handle_client
-} // struct StatsdTcpListener
+    }
+}
